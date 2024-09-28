fb5183ae9821b4a16cf4b2a2eed6e2669ac1b88c
Alexander Vasyliev <saddon11@gmail.com>
2024-09-28 18:33:28 +0200

    done: prep ex

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,68 @@
-const express = require('express')
+const express = require("express");
+const fs = require("fs");
 const app = express();
- 
 
-// YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.use(express.json());
+
+const postsFolder = "./posts/";
+
+if (!fs.existsSync(postsFolder)) {
+  fs.mkdirSync(postsFolder);
+}
+
+app.post("/blogs", (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  fs.writeFileSync(postsFolder + title, content);
+  res.end("ok");
+});
+
+app.put("/posts/:title", (req, res) => {
+  const titleBody = req.body.title;
+  const contentBody = req.body.content;
+  const title = req.params.title;
+
+  if (!titleBody || !contentBody) {
+    return res.status(500).end(`title and content are required.`);
+  }
+
+  if (fs.existsSync(postsFolder + title)) {
+    fs.unlinkSync(postsFolder + title);
+    fs.writeFileSync(postsFolder + titleBody, contentBody);
+    res.end("ok");
+  } else {
+    return res.status(404).end(`Post ${title} does not exist.`);
+  }
+});
+
+app.delete("/posts/:title", (req, res) => {
+  const title = req.params.title;
+
+  if (fs.existsSync(postsFolder + title)) {
+    fs.unlinkSync(postsFolder + title);
+    res.end("ok");
+  } else {
+    return res.status(404).end(`Post ${title} does not exist.`);
+  }
+});
+
+app.get("/blogs/:title", (req, res) => {
+  const title = req.params.title;
+  if (fs.existsSync(postsFolder + title)) {
+    const post = fs.readFileSync(postsFolder + title);
+    res.send(post);
+  } else {
+    return res.status(404).end(`Post ${title} does not exist.`);
+  }
+});
+
+app.get("/blogs", (req, res) => {
+  const posts = fs.readdirSync(postsFolder);
+  const result = posts.map((post) => {
+    return { title: post };
+  });
+  res.send(result);
+});
+
+app.listen(3000);
