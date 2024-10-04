280d4ebfa718bc301e94db1af05f408c22650649
Alexander Vasyliev <saddon11@gmail.com>
2024-10-04 20:23:18 +0200

    done: prep ex

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,15 +1,105 @@
-import express from 'express';
-// TODO Use below import statement for importing middlewares from users.js for your routes
-// TODO import { ....... } from "./users.js";
+import express from "express";
+import bcrypt from "bcrypt";
+import { findUserById, findUserByUsername, storeUser } from "./users.js";
+import jwt from "jsonwebtoken";
 
 let app = express();
 
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "randomString";
 app.use(express.json());
-// TODO: Create routes here, e.g. app.post("/register", .......)
 
 // Serve the front-end application from the `client` folder
-app.use(express.static('client'));
+app.use(express.static("client"));
+
+app.post("/auth/register", (req, res) => register(req, res));
+app.post("/auth/login", (req, res) => login(req, res));
+app.get("/auth/profile", (req, res) => profile(req, res));
+app.post("/auth/logout", (req, res) => logout(req, res));
+
+const register = async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    res.status(400).send({ message: "Username and password required!" });
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = storeUser(username, hashedPassword);
+
+  res.send({ id: user.id, username: user.username });
+};
+
+const login = async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    res.status(400).send({ message: "Username and password required!" });
+    return;
+  }
+
+  const user = findUserByUsername(username);
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    res.status(400).send({ message: "Invalid credentials!" });
+    return;
+  }
+
+  const token = jwt.sign({ id: user.id }, ACCESS_TOKEN_SECRET);
+  res.status(201).send({ token });
+};
+
+const profile = (req, res) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    res.status(401).send({ message: "JWT token is required!" });
+    return;
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const verifiedUser = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+    if (!verifiedUser) {
+      throw new Error();
+    }
+
+    const user = findUserById(verifiedUser.id);
+    res.send({ message: user.username });
+  } catch (error) {
+    res.status(401).send({ message: "Invalid credentials!" });
+    return;
+  }
+};
+
+const logout = (req, res) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    res.status(401).send({ message: "JWT token is required!" });
+    return;
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const verifiedUser = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+    if (!verifiedUser) {
+      throw new Error();
+    }
+
+    const user = findUserById(verifiedUser.id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(401).send({ message: "Invalid credentials!" });
+    return;
+  }
+};
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
--- week3/prep-exercise/server/users.js
+++ week3/prep-exercise/server/users.js
@@ -1,12 +1,24 @@
-import newDatabase from './database.js'
+import newDatabase from "./database.js";
 
 // Change this boolean to true if you wish to keep your
 // users between restart of your application
-const isPersistent = false
-const database = newDatabase({isPersistent})
+const isPersistent = false;
+const database = newDatabase({ isPersistent });
 
 // Create middlewares required for routes defined in app.js
 // export const register = async (req, res) => {};
 
 // You can also create helper functions in this file to help you implement logic
 // inside middlewares
+
+export const storeUser = (username, password) => {
+  return database.create({ username, password });
+};
+
+export const findUserByUsername = (username) => {
+  return database.getByUsername(username);
+};
+
+export const findUserById = (id) => {
+  return database.getById(id);
+};
